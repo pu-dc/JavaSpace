e6af7c7aa81d136e647a5c12fef0fd98283067d0
4rcherry <bilawalfr@gmail.com>
2018-09-11 01:42:30 +0700

    Add files via upload

---
+++ SystemProperties.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author 4rcherry
+ * 2018 © PUDC
+**/
+
+public class SystemProperties {
+    public SystemProperties() {}
+    public static void main(String args[]) {
+        System.getProperties().list(System.out);
+    }
+}
