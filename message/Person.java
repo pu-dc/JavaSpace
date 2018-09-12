99757b9ca88aa4ef1fdf04235ddcccd12aa7f155
4rcherry <bilawalfr@gmail.com>
2018-09-12 15:04:57 +0700

    Create Person.java

---
+++ message/Person.java
@@ -0,0 +1,67 @@
+package message;
+
+public class Person {
+	protected String name;
+	protected String address;
+	protected String city;
+	protected int age;
+
+	// Creates a new instance of Person
+	public Person() {
+		name 	= "";
+		address = "";
+		city 	= "";
+		age 	= 0;
+	}
+	public Person(String newName, String newAddress, String newCity, int newAge) {
+		name 	= newName;
+		address = newAddress;
+		city 	= newCity;
+		age 	= newAge;
+	}
+	public void setName(String newName) {
+		name = newName;
+	}
+	public void setAddress(String newAddress) {
+		address = newAddress;
+	}
+	public void setCity(String newCity) {
+		city = newCity;
+	}
+	public void setAge(int newAge) {
+		age = newAge;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public int getAge() {
+		return age;
+	}
+	public String toString() {
+		String str = 
+			"Name \t\t: " + getName() + "\n" +
+			"Address \t: " + getAddress() + "\n" +
+			"City \t\t: " + getCity() + "\n" +
+			"Age \t\t: " + getAge() + "\n";
+		return str;
+	}
+
+	static void ui() {
+		Person p = new Person("Bil Awal", "Kp. Pacing RT 03/06 Waringinjaya", "Bekasi", 20);
+		System.out.println("Bil Awal sebagai Person :");
+		System.out.println(p.getName());
+		System.out.println(p.getAddress());
+		System.out.println(p.getCity());
+		System.out.println(p.getAge());
+		System.out.println(p);
+	}
+	public static void main(String[] args) {
+		ui();
+	}
+}
