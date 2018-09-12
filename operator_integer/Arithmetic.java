03e9836c7249bcd01943a608cb768aeb54854042
4rcherry <bilawalfr@gmail.com>
2018-09-12 15:15:44 +0700

    Create Arithmetic.java

---
+++ operator_integer/Arithmetic.java
@@ -0,0 +1,15 @@
+package operator_integer;
+ 
+public class Arithmetic {
+	public static void main(String[] args) {
+		int x = 17, y = 5;
+
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+		System.out.println("x + y = " + (x + y));
+		System.out.println("x - y = " + (x - y));
+		System.out.println("x * y = " + (x * y));
+		System.out.println("x / y = " + (x / y));
+		System.out.println("x % y = " + (x % y));
+	}
+}
