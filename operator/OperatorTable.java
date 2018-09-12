6c655ab7fc83ff5d528b45dbbfc7d06130f80be4
4rcherry <bilawalfr@gmail.com>
2018-09-12 15:14:00 +0700

    Create OperatorTable.java

---
+++ operator/OperatorTable.java
@@ -0,0 +1,31 @@
+/*
+Operator menspesifikasikan evaluasi atau komputasi data. Operan yang dioperasikan dapat berupa literal, variable, atau nilai yang dikirim metode/fungsi.
+
+Berikut adalah table operator yang dipakai pada Java :
+
+	+		-		*		/		%		&		|
+
+	^		~		&&		||		!		<		>
+
+	<=		>=		<<		>>		>>>		=		?
+
+	++		--		==		+=		-=		*=		/=
+
+	%=		&=		|=		^=		!=		<<=		>>=
+
+	>>>=	.		[		]		(		)
+
+Operator memungkinkan kita melakukan evaluasi atau komputasi. Operator diterapkan ke variable dan literal membentuk ekspresi. Ekspresi dapat dipandang sebagai persamaan. Ekspresi adalah barisan operan-operan dan satu operator atau lebih yang menghasilkan hasil.
+
+Contoh eksresi sebagai berikut :
+	x = y / 3;
+
+Pada ekspresi diatas :
+	x dan y adalah variable
+	3 adalah literal
+	= dan / adalah operator
+
+Ekspresi ini adalah variable y dibagi 3 menggunakan operasi pembagian (/), dan hasilnya disimpan di x menggunakan operator pemberian nilai (=).
+
+Eksresi pemberian nilai dideskripsikan dari kanan ke kiri. Meskipun pendekatan analisis dari kanan ke kiri berguna dalam menunjukan operasi pemberian nilai, tetapi sebagian besar ekspresi dievaluasi dari kiri ke kanan.
+*/
